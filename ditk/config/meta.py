f9685cd7526fe710fc50f05d5efad170d35d021c
HansBug <hansbug@buaa.edu.cn>
2022-11-09 14:00:51 +0800

    release(hansbug): use version v0.1.0

--- ditk/config/meta.py
+++ ditk/config/meta.py
@@ -7,7 +7,7 @@ Overview:
 __TITLE__ = "DI-toolkit"
 
 #: Version of this project.
-__VERSION__ = "0.0.2"
+__VERSION__ = "0.1.0"
 
 #: Short description of the project, will be included in ``setup.py``.
 __DESCRIPTION__ = 'A simple tool for automatic parameter tuning.'
