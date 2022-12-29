73f26f754d68bc917ef9bc7c9d5dd1ad1ba36581
niuyazhe <niuyazhe@sensetime.com>
2022-12-29 17:16:47 +0800

    fix(nyz): fix unittest relative path bug

--- test/annonated/test_generate.py
+++ test/annonated/test_generate.py
@@ -2,7 +2,7 @@ import pytest
 import os
 import time
 from ditk.annonated import generate_annonated_doc
-from .ppo import ppo_policy_error
+from ditk.annonated.ppo import ppo_policy_error
 
 
 @pytest.mark.unittest
@@ -10,6 +10,7 @@ def test_generate():
     os.popen('rm -rf ppo.html')
     time.sleep(0.2)
     assert not os.path.exists('ppo.html')
-    generate_annonated_doc('ppo.py', 'ppo.html')
+    test_file_path = os.path.join(os.path.dirname(__file__), '../../ditk/annonated/ppo.py')
+    generate_annonated_doc(test_file_path, 'ppo.html')
     assert os.path.exists('ppo.html')
     os.popen('rm -rf ppo.html')
