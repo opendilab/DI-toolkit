7a54dc51fc5d9f45e604138632150168288e2815
HansBug <hansbug@buaa.edu.cn>
2023-05-25 15:06:21 +0800

    dev(hansbug): add cli test

--- test/doc/annotated/test_generate.py
+++ test/doc/annotated/test_generate.py
@@ -1,9 +1,11 @@
+import glob
 import os
 
 import pytest
-from hbutils.testing import isolated_directory
+from hbutils.testing import isolated_directory, simulate_entry
 
 from ditk.doc.annotated import generate_annotated_doc
+from ditk.doc.annotated.__main__ import cli
 
 
 @pytest.mark.unittest
@@ -11,3 +13,13 @@ def test_generate():
     with isolated_directory({'ppo.py': os.path.join('ditk', 'doc', 'annotated', 'ppo.py')}):
         generate_annotated_doc('ppo.py', 'ppo.html', 'This is the title')
         assert os.path.exists('ppo.html')
+        assert len(glob.glob(os.path.join('assets', '*.css'))) == 2
+
+
+@pytest.mark.unittest
+def test_cli():
+    with isolated_directory({'ppo.py': os.path.join('ditk', 'doc', 'annotated', 'ppo.py')}):
+        result = simulate_entry(cli, ['ditk.doc.annotated', 'create', '-i', 'ppo.py', '-o', 'ppo.html'])
+        assert result.exitcode == 0
+        assert os.path.exists('ppo.html')
+        assert len(glob.glob(os.path.join('assets', '*.css'))) == 2
